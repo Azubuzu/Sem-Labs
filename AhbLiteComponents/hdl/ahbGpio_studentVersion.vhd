6e86fde4adcb534aae514e46da81306f2cb72bd6
Azubuzu <azuria.ng@gmail.com>
2018-03-26 16:20:08 +0200

    26 mars commit

--- AhbLiteComponents/hdl/ahbGpio_studentVersion.vhd
+++ AhbLiteComponents/hdl/ahbGpio_studentVersion.vhd
@@ -19,9 +19,9 @@
 --
 ARCHITECTURE studentVersion OF ahbGpio IS
 	-- Constants
-	constant ioOutAddress natural := 0;
-	constant ioEnAddress natural := 1;
-	constant regLength natural := 2;
+	constant ioOutAddress : natural := 0;
+	constant ioEnAddress : natural := 1;
+	constant regLength : natural := 2;
 	
 --	signal enableReg0 : std_ulogic <= '0';
 --	signal enableReg1 : std_ulogic <= '0';
@@ -31,7 +31,7 @@ ARCHITECTURE studentVersion OF ahbGpio IS
 	signal ens : en_type;
 	
 	-- Register table
-	type reg_type is array (0 to regLength-1) of std_ulogic_vector(hWData'range);
+	type reg_type is array (0 to regLength-1) of std_ulogic_vector(ioEn'range);
 	signal regs : reg_type;
 	
 BEGIN
@@ -39,16 +39,19 @@ BEGIN
 	-- Enable definitions
 	enables: process(hAddr, hWrite, hSel)
 	begin
-		-- If write is activated and this peripheral is selected
-		if hWrite='1' and hSel='1' then
-			-- Got through enablesand activate only the right one
-			for i in range regs'low to regs'high loop
-				if hAddr = i then
-					ens(i) <= '1';
-				else
-					ens(i) <= '0';
-				end if;
-			end loop;
+		if hTrans(hTrans'left) = '1' and hSel='1' then
+			-- If write is activated
+			if hWrite='1' then
+				-- Got through enables and activate only the right one
+				for i in 0 to regLength-1 loop
+					if hAddr = i then
+						ens(i) <= '1';
+					else
+						ens(i) <= '0';
+					end if;
+				end loop;
+			end if;
+		end if;
 	end process enables;
 	
 	
@@ -57,34 +60,36 @@ BEGIN
 	begin
 		-- Reset
 		if hReset_n = '1' then
-			-- Set all regs to 0 at start
-			for i in regs'low to regs'high loop
+			-- Set all regs to 0 at start	
+		
+			for i in 0 to regLength-1 loop
 				regs(i) <= (others=>'0');
 			end loop;
-		
+			
 		-- Every clock 
 		elsif rising_edge(hClk) then
+
 			-- Go through registers to see if it is selected
-			for i in regs'low to regs'high loop
+			for i in 0 to regLength-1 loop
 				if ens(i)='1' then 
 					-- If so, write to this register
-					regs(i) <= hWData;
+					regs(i) <= hWData(ioOut'range);
 				end if;
 			end loop;
 		end if;
 	end process d_reg0;
 	
   -- AHB-Lite
-  hReady  <=	'0';	
+  hReady  <=	'1';	
   hResp	  <=	'0';	
 
   -- Out
-  ioOut <= reg(ioOutAddress);
-  ioEn  <= reg(ioEnAddress); 
+  ioOut <= regs(ioOutAddress);
+  ioEn  <= regs(ioEnAddress);
   
-  hRData  <=	ioIn; 
-  
-  
+  --hRData  <= resize(unsigned(ioIn),hRData'length); 
+  hRData(ioIn'range) <= ioIn;
+  hRData(hRData'high downto ioIn'length) <= (others=>'0');
 
 END ARCHITECTURE studentVersion;
 
