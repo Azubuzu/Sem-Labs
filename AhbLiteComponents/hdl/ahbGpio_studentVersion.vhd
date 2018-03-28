5ed734cfc55947be0758a8219a05d2ce777b61af
Azubuzu <azuria.ng@gmail.com>
2018-03-28 11:43:46 +0200

    Labo 05 - Finished
    GPIO

--- AhbLiteComponents/hdl/ahbGpio_studentVersion.vhd
+++ AhbLiteComponents/hdl/ahbGpio_studentVersion.vhd
@@ -18,13 +18,16 @@
 -- 00, data register provides the values detected on the lines.
 --
 ARCHITECTURE studentVersion OF ahbGpio IS
-	-- Constants
+
+	-- Defines register address positions 
 	constant ioOutAddress : natural := 0;
 	constant ioEnAddress : natural := 1;
+	
+	-- Number of registers
 	constant regLength : natural := 2;
 	
---	signal enableReg0 : std_ulogic <= '0';
---	signal enableReg1 : std_ulogic <= '0';
+	-- How many bits of the address we actually decode
+	constant addressBitsUsed : natural := 1;
 	
 	-- Enable signals for register writing
 	type en_type is array (0 to regLength-1) of std_ulogic;
@@ -36,20 +39,19 @@ ARCHITECTURE studentVersion OF ahbGpio IS
 	
 BEGIN
 	
-	-- Enable definitions
-	enables: process(hAddr, hWrite, hSel)
+	-- Enable definitions with pipeline
+	enables: process(hReset_n, hClk)
 	begin
-		if hTrans(hTrans'left) = '1' and hSel='1' then
-			-- If write is activated
-			if hWrite='1' then
-				-- Got through enables and activate only the right one
-				for i in 0 to regLength-1 loop
-					if hAddr = i then
-						ens(i) <= '1';
-					else
-						ens(i) <= '0';
-					end if;
-				end loop;
+		if hReset_n = '0' then
+			ens <= (others => '0');
+		elsif rising_edge(hClk) then
+			ens <= (others => '0');
+			if hTrans = transNonSeq and hSel='1' then
+				-- If write is activated
+				if hWrite='1' then
+					--ens(to_integer(hAddr))<='1;
+					ens(to_integer(hAddr(addressBitsUsed-1 downto 0))) <= '1';
+				end if;
 			end if;
 		end if;
 	end process enables;
@@ -59,19 +61,15 @@ BEGIN
 	d_reg0: process(hReset_n, hClk)
 	begin
 		-- Reset
-		if hReset_n = '1' then
+		if hReset_n = '0' then
 			-- Set all regs to 0 at start	
-		
-			for i in 0 to regLength-1 loop
-				regs(i) <= (others=>'0');
-			end loop;
+			regs <= (others=>(others=>'0'));
 			
 		-- Every clock 
 		elsif rising_edge(hClk) then
-
 			-- Go through registers to see if it is selected
 			for i in 0 to regLength-1 loop
-				if ens(i)='1' then 
+				if ens(i) = '1' then 
 					-- If so, write to this register
 					regs(i) <= hWData(ioOut'range);
 				end if;
