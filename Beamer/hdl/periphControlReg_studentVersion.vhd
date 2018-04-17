dfb189a732a02a4ef34dfd4f736584c3b13d05c9
Azubuzu <azuria.ng@gmail.com>
2018-04-17 11:45:38 +0200

    Labo 07 suite

--- Beamer/hdl/periphControlReg_studentVersion.vhd
+++ Beamer/hdl/periphControlReg_studentVersion.vhd
@@ -1,6 +1,42 @@
 ARCHITECTURE studentVersion OF periphControlReg IS
+
+	constant runBit : natural := 0; 
+	constant updateBit : natural := 1; 
+	constant interpolateBit : natural := 2; 
+	constant bitsUsed : natural := 3;
+
+	signal data : unsigned(dataBitNb-1 downto 0);
+	
 BEGIN
-  run               <= '0';
-  updatePattern     <= '0';
-  interpolateLinear <= '0';
-END ARCHITECTURE studentVersion;
+
+	-- D flip flop controlling register values
+	register_flip_flop: process(clock, reset)
+	begin
+	if reset='0' then
+
+		-- Reset register
+		data <= (others=>'0');
+
+	elsif rising_edge(clock) then
+		
+		-- Only if this device is selected
+		if en='1' then
+
+			-- Write / read
+			if write='1' then
+				data <= unsigned(dataIn);
+			else
+				dataOut <= std_logic_vector(data);
+			end if;
+			
+		end if;
+	end if;
+	end process register_flip_flop;
+
+	-- Set outputs
+	run               <= data(runBit);
+	updatePattern     <= data(updateBit);
+	interpolateLinear <= data(interpolateBit);
+	patternSize 	  <= data(data'high downto bitsUsed);
+	
+  END ARCHITECTURE studentVersion;
--- Beamer/hdl/periphSpeedReg_studentVersion.vhd
+++ Beamer/hdl/periphSpeedReg_studentVersion.vhd
@@ -1,5 +1,34 @@
 ARCHITECTURE studentVersion OF periphSpeedReg IS
+
+	signal data : unsigned(dataBitNb-1 downto 0);
+	
 BEGIN
-  updatePeriod <= (others => '0');
+
+	-- D flip flop controlling register values
+	register_flip_flop: process(clock, reset)
+	begin
+	if reset='0' then
+
+		-- Reset register
+		data <= (others=>'0');
+
+	elsif rising_edge(clock) then
+		
+		-- Only if this device is selected
+		if en='1' then
+
+			-- Write / read
+			if write='1' then
+				data <= unsigned(dataIn);
+			else
+				dataOut <= std_logic_vector(data);
+			end if;
+			
+		end if;
+	end if;
+	end process register_flip_flop;
+
+	updatePeriod <= data;
+	
 END ARCHITECTURE studentVersion;
 
