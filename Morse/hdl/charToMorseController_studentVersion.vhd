677cd5296faffc224d1339889d7329a847a999b2
Azubuzu <azuria.ng@gmail.com>
2018-04-25 11:46:29 +0200

    Started Labo 11

---
+++ Morse/hdl/charToMorseController_studentVersion.vhd
@@ -0,0 +1,13 @@
+--
+-- VHDL Architecture Morse.charToMorseController.studentVersion
+--
+-- Created:
+--          by - leo.dosreis.UNKNOWN (WE5400)
+--          at - 11:19:15 25.04.2018
+--
+-- using Mentor Graphics HDL Designer(TM) 2015.2 (Build 5)
+--
+ARCHITECTURE studentVersion OF charToMorseController IS
+BEGIN
+END ARCHITECTURE studentVersion;
+
